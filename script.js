496208149baa7bbe0a4be72372ac971d53809f0e
Amir Karimian <akarimian759@gmail.com>
2026-09-20 10:50:55 +0330

    show random color

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-"use strict"
+"use strict";
+
+const colors = [
+  "#ffbe0b",
+  "#fb5607",
+  "#e71d36",
+  "#ff006e",
+  "#8338ec",
+  "#3a86ff",
+  "#62c2b1",
+  "#16db65",
+];
+
+const coloredSquares = document.querySelectorAll(".colored-square");
+
+function showRandomColor() {
+  const random = Math.floor(Math.random() * colors.length);
+  coloredSquares.forEach(square => {
+    square.style.backgroundColor = colors[random]
+  });
+}
+showRandomColor()
